fe69595d82adb5afd00cb68718dcd9ea32caec67
Harry Moss <h.moss@ucl.ac.uk>
2023-11-09 17:12:23 +0000

    Add basic openai API calling backend

---
+++ src/llm_api/backends/openai.py
@@ -0,0 +1,105 @@
+"""Provides user search processing and OpenAI language model calling functionality."""
+import json
+
+import openai
+from openai import OpenAI
+
+from llm_api.config import Settings
+
+
+class OpenaiModelCallError(Exception):
+    """Generate a custom exception for Openai API errors."""
+
+
+class OpenaiCaller:
+    """Process prompts and call Openai LLMs."""
+
+    def __init__(self, settings: Settings) -> None:
+        """
+        Class constructor.
+
+        Args:
+            settings (Settings): Pydantic settings object.
+        """
+        self.settings = settings
+
+        self.client = self.get_client()
+
+    def get_client(self) -> OpenAI:
+        """
+        Retrieve an OpenAI client object.
+
+        Returns
+            OpenAI: OpenAI client object
+        """
+        return OpenAI(api_key=self.settings.openai_api_key.get_secret_value())
+
+    @staticmethod
+    def generate_openai_prompt(user_input: str) -> list[dict[str, str]]:
+        """
+        Generate a prompt from user input to send to Openai models.
+
+        The prompt includes `system` and `user` roles to define expected
+        input and output formats.
+
+        Args:
+            user_input (str): User search input.
+
+        Returns:
+            list[dict[str, str]]: A list of dictionaries containing roles and content.
+        """
+        return [
+            {
+                "role": "system",
+                "content": "You are a helpful search assistant that extracts \
+                    useful entities and relationships from user search queries.",
+            },
+            {
+                "role": "system",
+                "content": "Users will provide you with a search, as a string. \
+                    You should examine this string and determine entities directly \
+                    from the string in addition to any other entities not found in \
+                    the string that may be relevant to the search.",
+            },
+            {
+                "role": "system",
+                "content": "You should provide your response as valid JSON.",
+            },
+            {"role": "user", "content": f"{user_input}"},
+        ]
+
+    def call_model(self, openai_model: str, prompt: list[dict]) -> dict:
+        """
+        Call the external Openai model specified with a defined prompt.
+
+        Args:
+            openai_model (str): String representing OpenAI model to call
+            prompt (list[dict]): Constructed prompt from user input
+
+        Raises:
+            OpenaiModelCallError: API connection exception
+            OpenaiModelCallError: Rate limit exception
+            OpenaiModelCallError: General API error exception
+
+        Returns:
+            dict: Dictionary created from JSON response
+        """
+        try:
+            response = self.client.chat.completions.create(
+                model=openai_model,
+                messages=prompt,
+                response_format={"type": "json_object"},
+            )
+            response_content = response.choices[0].message.content
+
+            return json.loads(response_content)
+
+        except openai.APIConnectionError as connection_error:
+            message = f"Unable to connect to OpenAI. {connection_error}"
+            raise OpenaiModelCallError(message) from connection_error
+        except openai.RateLimitError as rate_limit_error:
+            message = f"Rate limit exceeded. {rate_limit_error}"
+            raise OpenaiModelCallError(message) from rate_limit_error
+        except openai.APIError as api_error:
+            message = f"OpenAI API error: {api_error}"
+            raise OpenaiModelCallError(message) from api_error
---
+++ tests/test_backends_openai.py
@@ -0,0 +1,147 @@
+import json
+
+import openai
+import pytest
+from openai.types.chat.chat_completion import ChatCompletion, Choice
+from openai.types.chat.chat_completion_message import ChatCompletionMessage
+
+from llm_api.backends.openai import OpenaiCaller, OpenaiModelCallError
+
+
+def test_openai_caller_load_settings(mocker, mock_settings):
+    mocked_openai_client = mocker.patch(
+        "llm_api.backends.openai.OpenaiCaller.get_client"
+    )
+
+    caller = OpenaiCaller(mock_settings)
+    expected_test_key = mock_settings.openai_api_key.get_secret_value()
+
+    assert caller.settings.openai_api_key.get_secret_value() == expected_test_key
+    mocked_openai_client.assert_called_once()
+
+
+def test_generate_openai_prompt_success():
+    user_input = "What day is it today?"
+
+    expected_output = {"role": "user", "content": user_input}
+
+    prompt_output = OpenaiCaller.generate_openai_prompt(user_input)
+
+    expected_prompt_elements = 4
+    assert isinstance(prompt_output, list)
+    assert len(prompt_output) == expected_prompt_elements
+    assert prompt_output[-1]["role"] == "user"
+    assert prompt_output[-1] == expected_output
+
+
+def test_call_model_success(mocker, mock_settings):
+    caller = OpenaiCaller(mock_settings)
+    test_model = "my-test-gpt-model"
+
+    mocked_client_call = mocker.patch.object(caller.client.chat.completions, "create")
+
+    expected_example_response_string = '{\n    "name": "William Shakespeare",\n \
+        "occupation": "Playwright, poet, actor"}'
+
+    mocked_client_call.return_value = ChatCompletion(
+        id="test-id",
+        choices=[
+            Choice(
+                finish_reason="stop",
+                index=0,
+                message=ChatCompletionMessage(
+                    content=expected_example_response_string,
+                    role="assistant",
+                    function_call=None,
+                    tool_calls=None,
+                ),
+            )
+        ],
+        created=1699546437,
+        model=test_model,
+        object="chat.completion",
+    )
+
+    test_prompt = [
+        {"role": "system", "content": "You are a test system"},
+        {"role": "system", "content": "Provide a valid JSON response to the user."},
+        {"role": "user", "content": "Who is Shakespeare?"},
+    ]
+
+    response = caller.call_model(test_model, test_prompt)
+
+    assert response == json.loads(expected_example_response_string)
+
+
+def test_call_model_failure_api_connection_error(mocker, mock_settings):
+    caller = OpenaiCaller(mock_settings)
+    test_model = "my-test-gpt-model"
+
+    mocked_client_call = mocker.patch.object(caller.client.chat.completions, "create")
+    expected_error_message = "Unable to connect to OpenAI. Connection error."
+    patched_httpx_request = mocker.patch("httpx.Request")
+    mocked_client_call.side_effect = openai.APIConnectionError(
+        request=patched_httpx_request
+    )
+
+    test_prompt = [
+        {"role": "system", "content": "You are a test system"},
+        {"role": "system", "content": "Provide a valid JSON response to the user."},
+        {"role": "user", "content": "Who is Shakespeare?"},
+    ]
+
+    with pytest.raises(OpenaiModelCallError) as exception:
+        caller.call_model(test_model, test_prompt)
+
+    assert str(exception.value) == expected_error_message
+
+
+def test_call_model_failure_rate_limit_error(mocker, mock_settings):
+    caller = OpenaiCaller(mock_settings)
+    test_model = "my-test-gpt-model"
+
+    mocked_client_call = mocker.patch.object(caller.client.chat.completions, "create")
+    unique_test_error_message = "This is a test case - rate limit error."
+    expected_error_message = f"Rate limit exceeded. {unique_test_error_message}"
+    mocked_response = mocker.patch("httpx.Response")
+
+    mocked_client_call.side_effect = openai.RateLimitError(
+        unique_test_error_message, response=mocked_response, body=None
+    )
+
+    test_prompt = [
+        {"role": "system", "content": "You are a test system"},
+        {"role": "system", "content": "Provide a valid JSON response to the user."},
+        {"role": "user", "content": "Who is Shakespeare?"},
+    ]
+
+    with pytest.raises(OpenaiModelCallError) as exception:
+        caller.call_model(test_model, test_prompt)
+
+    assert str(exception.value) == expected_error_message
+
+
+def test_call_model_failure_api_error(mocker, mock_settings):
+    caller = OpenaiCaller(mock_settings)
+    test_model = "my-test-gpt-model"
+
+    mocked_client_call = mocker.patch.object(caller.client.chat.completions, "create")
+    unique_test_error_message = "This is test case - API Error"
+    expected_error_message = f"OpenAI API error: {unique_test_error_message}"
+
+    mocked_request = mocker.patch("httpx.Request")
+
+    mocked_client_call.side_effect = openai.APIError(
+        unique_test_error_message, request=mocked_request, body=None
+    )
+
+    test_prompt = [
+        {"role": "system", "content": "You are a test system"},
+        {"role": "system", "content": "Provide a valid JSON response to the user."},
+        {"role": "user", "content": "Who is Shakespeare?"},
+    ]
+
+    with pytest.raises(OpenaiModelCallError) as exception:
+        caller.call_model(test_model, test_prompt)
+
+    assert str(exception.value) == expected_error_message
