d95dc5139e717019eb3fe2c7938bff143aad8bfd
Harry Moss <h.moss@ucl.ac.uk>
2023-11-22 11:35:59 +0000

    Adding a basic gunicorn config to allow uvicorn logs to pass through - only applies to the gunicorn server version of the app via docker compose

--- src/llm_api/gunicorn_conf.py
+++ src/llm_api/gunicorn_conf.py
@@ -1,9 +1,11 @@
+# ruff: noqa
 """Define Gunicorn config."""
 
-from loguru import logger
 import logging
-import sys
 import multiprocessing
+import sys
+
+from loguru import logger
 
 bind = "0.0.0.0:8000"
 
@@ -16,41 +18,47 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # threads per worker
 threads = 1
 
+logging_level = "INFO"
+
+
 class InterceptHandler(logging.Handler):
     def emit(self, record):
         try:
             level = logger.level(record.levelname).name
         except ValueError:
             level = record.levelno
-        
+
         frame = logging.currentframe()
         depth = 2
         while frame.f_code.co_filename == logging.__file__:
             frame = frame.f_back
             depth += 1
-        
+
         logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
 
-# Replace Gunicorn loggers with Loguru
-gunicorn_logger = logging.getLogger('gunicorn.error')
+
+# Replace Gunicorn loggers with Loguru logger
+gunicorn_logger = logging.getLogger("gunicorn.error")
 gunicorn_logger.handlers = [InterceptHandler()]
 gunicorn_logger.propagate = False
 
-access_logger = logging.getLogger('gunicorn.access')
+access_logger = logging.getLogger("gunicorn.access")
 access_logger.handlers = [InterceptHandler()]
 access_logger.propagate = False
 
-logger.add(sys.stderr, format="{time} {level} {message}", level="INFO")
+# Remove any other loggers handled by loguru to eliminate duplicate logging
+logger.remove()
+# Add the desired logs in the desired format, at the desired level
+logger.add(sys.stderr, format="{time} {level} {message}", level=logging_level)
+
 
 def when_ready(server):
     logger.info("Server ready. Spawning workers.")
 
+
 def on_starting(server):
     logger.info("Starting Gunicorn.")
 
+
 def on_exit(server):
     logger.info("Server shutting down.")
-
-raw_env = [
-    "GUNICORN_CMD_ARGS=--access-logformat=%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s'",
-]
