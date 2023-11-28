132058fbe54851eeee70fa2258e710e4b7812e6d
Harry Moss <h.moss@ucl.ac.uk>
2023-11-28 10:36:09 +0000

    Add mocked settings and update .secrets.baseline

--- tests/fixtures/backend.py
+++ tests/fixtures/backend.py
@@ -11,4 +11,10 @@ def mock_openai_client(mocker):
 
 @pytest.fixture()
 def mock_settings():
-    return Settings(openai_api_key="test_fixture_key", openai_llm_name="test-fixture-model")
+    return Settings(
+        openai_api_key="test_fixture_key",
+        openai_llm_name="test-fixture-model",
+        aws_access_key_id="dummy-access-id",
+        aws_secret_access_key="dummy-secret-key",
+        aws_bedrock_model_id="test-bedrock-model"
+    )
